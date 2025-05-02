380f90dea97e2751166950564f7593a404ee413a
Nugi <susatos9@gmail.com>
2025-05-02 14:49:13 +0700

    Finished compilation script

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,3 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello, world!");
+console.log("This is a TypeScript file.");
+console.log("It will be compiled to JavaScript.");
+console.log("You can run it using Node.js.");
+console.log("Have a great day!");
 //# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+
+console.log("Hello, world!");
+console.log("This is a TypeScript file.");
+console.log("It will be compiled to JavaScript.");
+console.log("You can run it using Node.js.");
+console.log("Have a great day!");
+
+
